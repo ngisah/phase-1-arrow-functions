70d27b45a1877fea09b080a9fb5e3d4ed856e4e5
edwin <ngisahedwin@gmail.com>
2022-08-17 17:04:06 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function (a, b){
+
+    return parseInt(a/b);
+}
+console.log(divide(2000, 100));
+
+const square = Number => Number ** 2;
+
+const add = (num1, num2) => num1 + num2;
+
